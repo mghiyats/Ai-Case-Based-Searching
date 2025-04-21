a135a82c40c92aa5a24173eded49ed82c5bb07cf
myousefg <myousefg@gmail.com>
2025-04-22 00:02:25 +0700

    Memberikan Catatan pada kode

--- searching.py
+++ searching.py
@@ -1,6 +1,9 @@
+ # Algorithm for Function Optimization
+ # Import library standard
 import random
 import math
 
+# Inisialisasi populasi
 POP_SIZE = 20
 BIT_LENGTH = 10
 CHROM_LENGTH = 2 * BIT_LENGTH
@@ -9,11 +12,13 @@ X_MIN, X_MAX = -10, 10
 CROSSOVER_RATE = 0.8
 MUTATION_RATE = 0.01
 
+# Dekode biner
 def decode_gene(gene):
     decimal = int(gene, 2)
     real = X_MIN + (decimal / (2**BIT_LENGTH - 1)) * (X_MAX - X_MIN)
     return real
 
+# Dekode kromosom
 def decode_chromosome(chrom):
     x1_bin = chrom[:BIT_LENGTH]
     x2_bin = chrom[BIT_LENGTH:]
@@ -21,6 +26,7 @@ def decode_chromosome(chrom):
     x2 = decode_gene(x2_bin)
     return x1, x2
 
+# Perhitungan fitness
 def fitness(chrom):
     x1, x2 = decode_chromosome(chrom)
     try:
@@ -29,26 +35,31 @@ def fitness(chrom):
     except:
         return 0.0
 
+# Generate random binary string
 def random_chrom():
     return ''.join(random.choice(['0', '1']) for _ in range(CHROM_LENGTH))
 
+# Pemilihan orangtua
 def select_parents(pop):
     candidates = random.sample(pop, 3)
     candidates.sort(key=lambda c: fitness(c), reverse=True)
     return candidates[0], candidates[1]
 
+# Crossover (pindah silang) 
 def crossover(parent1, parent2):
     if random.random() < CROSSOVER_RATE:
         point = random.randint(1, CHROM_LENGTH - 1)
         return parent1[:point] + parent2[point:], parent2[:point] + parent1[point:]
     return parent1, parent2
 
+# Mutasi
 def mutate(chrom):
     return ''.join(
         bit if random.random() > MUTATION_RATE else ('1' if bit == '0' else '0')
         for bit in chrom
     )
 
+# Pergantian generasi
 def run_ga():
     population = [random_chrom() for _ in range(POP_SIZE)]
     best_solution = max(population, key=fitness)
@@ -68,10 +79,13 @@ def run_ga():
 
     x1, x2 = decode_chromosome(best_solution)
     final_value = - (math.sin(x1) * math.cos(x2) * math.tan(x1 + x2) + 0.75 * math.exp(1 - math.sqrt(x1**2)))
-    print("Case Based Searching (NIM 1302220085 - 1302220109)")
+
+    # Hasil Akhir
+    print("\nCase Based Searching (NIM 1302220085 - 1302220109)")
     print("\nBest Chromosome:", best_solution)
     print("Nilai x1 =", x1)
     print("Nilai x2 =", x2)
+    print("\n Data Tambahan:")
     print("Nilai f(x1,x2) =", final_value)
     print("Fitness akhir =", fitness(best_solution))
 
